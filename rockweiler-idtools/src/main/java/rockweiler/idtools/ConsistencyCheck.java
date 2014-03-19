d7b64fdff96a211649575114f9ee34041e0a110d
Danil Suits <danil@vast.com>
2014-03-18 21:19:17 -0500

    Minor changes for idtools

---
+++ rockweiler-idtools/src/main/java/rockweiler/idtools/ConsistencyCheck.java
@@ -0,0 +1,12 @@
+/**
+ * Copyright Vast 2014. All Rights Reserved.
+ *
+ * http://www.vast.com
+ */
+package rockweiler.idtools;
+
+/**
+ * @author Danil Suits (danil@vast.com)
+ */
+public class ConsistencyCheck {
+}

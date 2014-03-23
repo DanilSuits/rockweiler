f674c8d41757af9479e68fd75e981730c1563dd6
Danil Suits <danil@vast.com>
2014-03-23 17:19:02 -0500

    reformat master database

---
+++ rockweiler-core/src/main/java/rockweiler/player/jackson/SimpleArchive.java
@@ -0,0 +1,12 @@
+/**
+ * Copyright Vast 2014. All Rights Reserved.
+ *
+ * http://www.vast.com
+ */
+package rockweiler.player.jackson;
+
+/**
+ * @author Danil Suits (danil@vast.com)
+ */
+public class SimpleArchive {
+}
